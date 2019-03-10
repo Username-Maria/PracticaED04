1c121ab89823cab5a305c5b7eb8053665e367513
Username-Maria <48414724+Username-Maria@users.noreply.github.com>
2019-03-10 20:22:47 +0100

    Add files via upload

--- Reloj.java
+++ Reloj.java
@@ -11,173 +11,99 @@ package practicaut3;
  */
 public class Reloj {
 
-    private int HH;
-
-    int HH,
-    private int MM;
-
     //Atributos de la clase Reloj
-    int HH, MM, SS;
-    private int dia;
-    int  ;
-    private int SS;
-    int dia;
-    int mes;
-    private int mes;
+    int hora, minuto, segundo;
 
     //Constructor con parámetros
-    public Reloj(int HH, int MM, int SS, int dia, int mes) {
-        this.HH = HH;
-        this.MM = MM;
-        this.SS = SS;
-        this.dia = dia;
-        this.mes = mes;
+    public Reloj(int hora, int minuto, int segundo) {
+        this.hora = hora;
+        this.minuto = minuto;
+        this.segundo = segundo;
     }
     //Métodos para asignar valores a los atributos hora, minuto y segundo
-
-    public void asignarHora(int HH) {
-        this.HH = HH;
-    }
-
-    public void asignarMinuto(int MM) {
-        this.MM = MM;
+    
+    public void asignarHora(int hora) {
+        this.hora = hora;
     }
 
-    public void asignarSegundo(int SS) {
-        this.SS = SS;
+    public void asignarMinuto(int minuto) {
+        this.minuto = minuto;
     }
+
     
-    public void asignarDia(int dia) {
-        this.dia= dia;
+    public void asignarSegundo(int segundo) {    
+        this.segundo = segundo;
     }
-    
-    public void asignarMes(int mes) {
-        this.mes = mes;
-    }
-    
-    //Métodos para devolver valores de los atributos hora, minuto y segundo
+ //Métodos para devolver valores de los atributos hora, minuto y segundo
 
     public int obtenerHora() {
-        return HH;
+        return hora;
     }
 
     public int obtenerMinuto() {
-        return MM;
+        return minuto;
     }
 
     public int obtenerSegundo() {
-        return SS;
+        return segundo;
     }
     
-    public int obtenerDia() {
-        return dia;
-    }
-    
-    public int obtenerMes() {
-        return mes;
-    }
-
     //Método que incrementa 1 segundo la hora
     public void incrementa() {
         boolean cambio_m, cambio_h;
-        cambio_m = (SS + 1 > 59) ? true : false;
-        cambio_h = ((cambio_m) && (MM + 1 > 59)) ? true : false;
-        MM = (cambio_m) ? MM + 1 : MM;
-        MM = (MM > 59) ? 0 : MM;
-        HH = (cambio_h) ? HH + 1 : HH;
-        HH = (HH > 23) ? 0 : HH;
-        SS = (SS + 1 > 59) ? 0 : SS + 1;
+        cambio_m = (segundo + 1 > 59) ? true : false;
+        cambio_h = ((cambio_m) && (minuto + 1 > 59)) ? true : false;
+        minuto = (cambio_m) ? minuto + 1 : minuto;
+        minuto = (minuto > 59) ? 0 : minuto;
+        hora = (cambio_h) ? hora + 1 : hora;
+        hora = (hora > 23) ? 0 : hora;
+        segundo = (segundo + 1 > 59) ? 0 : segundo + 1;
     }
-
     //Método que decrementa un segundo la hora
     public void decrementa() {
         boolean cambio_m, cambio_h;
 
-        cambio_m = (SS - 1 < 0) ? true : false;
-        cambio_h = ((cambio_m) && (MM - 1 < 0)) ? true : false;
+        cambio_m = (segundo - 1 < 0) ? true : false;
+        cambio_h = ((cambio_m) && (minuto - 1 < 0)) ? true : false;
 
-        MM = (cambio_m) ? MM - 1 : MM;
-        MM = (MM < 0) ? 59 : MM;
-        HH = (cambio_h) ? HH - 1 : HH;
-        HH = (HH < 0) ? 23 : HH;
+        minuto = (cambio_m) ? minuto - 1 : minuto;
+        minuto = (minuto < 0) ? 59 : minuto;
+        hora = (cambio_h) ? hora - 1 : hora;
+        hora = (hora < 0) ? 23 : hora;
 
-        SS = (SS - 1 < 0) ? 59 : SS - 1;
+        segundo = (segundo - 1 < 0) ? 59 : segundo - 1;
 
     }
-
     //Método que da la hora en formato 12H
     public String hora12() {
-
+        
         String mediodia, cadena;
         int hora12;
-        mediodia = (HH >= 12) ? "PM" : "AM";
-        hora12 = (HH > 12) ? HH - 12 : HH;
+        mediodia = (hora >= 12) ? "PM" : "AM";
+        hora12 = (hora > 12) ? hora - 12 : hora;
 
-        //Forma de poner un 0 en los minutos y segundos si es menor de 10
+    //Forma de poner un 0 en los minutos y segundos si es menor de 10
         cadena = hora12 + ":";
-        cadena += (MM < 10) ? "0" : "";
-        cadena += MM + ":";
-        cadena += (SS < 10) ? "0" : "";
-        cadena += SS;
+        cadena += (minuto < 10) ? "0" : "";
+        cadena += minuto + ":";
+        cadena += (segundo < 10) ? "0" : "";
+        cadena += segundo;
         cadena += " " + mediodia;
         return cadena;
 
     }
-
     //Método que da la hora en formato 24H
     public String hora24() {
-
+        
         String cadena;
 
         //Forma de poner un 0 en los minutos y segundos si es menor de 10
-        cadena = HH + ":";
-        cadena += (MM < 10) ? "0" : "";
-        cadena += MM + ":";
-        cadena += (SS < 10) ? "0" : "";
-        cadena += SS;
+        cadena = hora + ":";
+        cadena += (minuto < 10) ? "0" : "";
+        cadena += minuto + ":";
+        cadena += (segundo < 10) ? "0" : "";
+        cadena += segundo;
         return cadena;
 
     }
-
-    //Metodos getter y setter
-    
-    public int getHH() {
-        return HH;
-    }
-
-    public void setHH(int HH) {
-        this.HH = HH;
-    }
-
-    public int getMM() {
-        return MM;
-    }
-
-    public void setMM(int MM) {
-        this.MM = MM;
-    }
-
-    public int getSS() {
-        return SS;
-    }
-
-    public void setSS(int SS) {
-        this.SS = SS;
-    }
-
-    public int getDia() {
-        return dia;
-    }
-
-    public void setDia(int dia) {
-        this.dia = dia;
-    }
-
-    public int getMes() {
-        return mes;
-    }
-
-    public void setMes(int mes) {
-        this.mes = mes;
-    }  
 }
